03aa261bb90c1d89b5f0a7c1d5299c6d96e5ea81
Peter M <pmoyano@solvd.com>
2023-04-11 16:10:36 -0300

    Create a new Photo entity

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,6 +2,8 @@ import "reflect-metadata";
 import { password } from "./secret";
 import { DataSource } from "typeorm";
 import { User } from "./entity/User";
+import { Photo } from "./entity/Photo";
+
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -12,7 +14,16 @@ export const AppDataSource = new DataSource({
   database: "typeorm-test",
   synchronize: true,
   logging: false,
-  entities: [User],
+  entities: [User, Photo],
   migrations: [],
   subscribers: [],
 });
+
+// to initialize initial connection with the database, register all entities
+// and "synchronize" database schema, call "initialize()" method of a newly created database
+// once in your application bootstrap
+AppDataSource.initialize()
+    .then(() => {
+        // here you can start to work with your database
+    })
+    .catch((error) => console.log(error))
